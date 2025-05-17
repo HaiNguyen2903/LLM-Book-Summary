d634609ff19ad3625cbc63659fe8323d077f255d
HaiNguyen2903 <hainguyen29031412@gmail.com>
2025-05-18 02:25:45 +1000

    remove exp files

--- create_database.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-# Connect to a database (creates 'books.db' if it doesn't exist)
-conn = sqlite3.connect('books.db')
-
-# Create a cursor object to execute SQL commands
-cursor = conn.cursor()
